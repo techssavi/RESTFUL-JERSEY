a62541e617b7648e0d11eaabb53b11efab4952f8
Savitha S <savitha.satyanarayan@gmail.com>
2016-05-22 11:30:46 +0530

    Add files via upload
    Jersey Example

---
+++ restful/src/main/java/ss/restful/example/Users.java
@@ -0,0 +1,22 @@
+package ss.restful.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import ss.restful.example.service.UserService;
+
+
+@Path("/users")
+public class Users implements UserService {
+
+	/* (non-Javadoc)
+	 * @see ss.restful.example.UserService#welcomeMessage(java.lang.String)
+	 */
+	@GET
+	@Path("/{name}")
+	public String welcomeMessage(@PathParam("name") String name) {
+		return "Hello!! " + name;
+	}
+	
+}
---
+++ restful/src/main/java/ss/restful/example/service/UserService.java
@@ -0,0 +1,8 @@
+package ss.restful.example.service;
+
+public interface UserService {
+
+	
+	String welcomeMessage(String name);
+
+}
